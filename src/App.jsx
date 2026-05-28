256ff005816e3b3771b2e90e7566c3d04de43613
mw006453-bit <mw006453@gmail.com>
2026-05-28 18:07:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,21 @@ export default function App() {
       <section className="section">
         <h1>HAPPY BIRTHDAYY 🎉</h1>
       </section>
+      {/* MUSIC AT TOP */}
+<iframe
+  width="100%"
+  height="120"
+  style={{
+    marginTop: "20px",
+    marginBottom: "40px",
+    borderRadius: "12px"
+  }}
+  src="https://www.youtube.com/embed/rHvQakk1zMA"
+  title="Laufey - From The Start"
+  frameBorder="0"
+  allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowFullScreen
+/>
 
       {/* SECTION 2 */}
       <section className="section">
